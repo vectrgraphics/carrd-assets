f35935cb4e6e9dba3c68e6603aa2994685746589
vectr <trevor.rendell@gmail.com>
2025-08-07 22:41:53 +1000

    made music-links drop down

--- js/script.js
+++ js/script.js
@@ -39,3 +39,35 @@ slides.forEach((slide, i) => {
     slide.classList.add('active');
   }
 });
+
+
+
+let touchStartX = 0;
+let touchEndX = 0;
+
+const swipeThreshold = 50; // Minimum px distance to qualify as swipe
+
+const carousel = document.querySelector('.carousel'); // or your outer container
+
+carousel.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+carousel.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  handleSwipeGesture();
+});
+
+function handleSwipeGesture() {
+  const deltaX = touchEndX - touchStartX;
+
+  if (Math.abs(deltaX) > swipeThreshold) {
+    if (deltaX > 0) {
+      // Swiped right
+      document.querySelector('.chevron.left').click();
+    } else {
+      // Swiped left
+      document.querySelector('.chevron.right').click();
+    }
+  }
+}
